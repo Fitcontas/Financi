ee6accadd74bc2f3461b8778d51678c8dfbc9470
Gilglécio Santos <gilglecio_765@hotmail.com>
2015-02-11 21:06:11 -0300

    adicionando fontes do cleanzone

--- apps/financi/templates/index.php
+++ apps/financi/templates/index.php
@@ -4,20 +4,24 @@
 	<meta charset="utf-8">
 	<meta content="width=device-width, initial-scale=1.0" name="viewport">
 	<title><?php echo isset($title) ? $title : 'Untitle' ?></title>
+
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,400italic,700,800' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Raleway:300,200,100' rel='stylesheet' type='text/css'>
+	
 	<?php if(isset($head_css)): ?>
-	<?php foreach ($head_css as $key => $value): ?>
-		<?php if(is_array($value)): ?>
-		<link rel="stylesheet" href="<?php echo HOST . DS . $key ?>" media="<?php $value['media'] ?>">
-		<?php else: ?>
-		<link rel="stylesheet" href="<?php echo HOST . DS . $value ?>">
-		<?php endif; ?>
-	<?php endforeach ?>
+		<?php foreach ($head_css as $key => $value): ?>
+			<?php if(is_array($value)): ?>
+			<link rel="stylesheet" href="<?php echo HOST . DS . $key ?>" media="<?php $value['media'] ?>">
+			<?php else: ?>
+			<link rel="stylesheet" href="<?php echo HOST . DS . $value ?>">
+			<?php endif; ?>
+		<?php endforeach ?>
 	<?php endif; ?>
 
 	<?php if(isset($head_js)): ?>
-	<?php foreach ($head_js as $js): ?>
-		<script type="text/javascript" src="<?php echo HOST . DS . $js ?>"></script>
-	<?php endforeach ?>
+		<?php foreach ($head_js as $js): ?>
+			<script type="text/javascript" src="<?php echo HOST . DS . $js ?>"></script>
+		<?php endforeach ?>
 	<?php endif; ?>
 </head>
 <body>
@@ -89,19 +93,19 @@
     
 
 	<?php if(isset($foot_css)): ?>
-	<?php foreach ($foot_css as $key => $value): ?>
-		<?php if(is_array($value)): ?>
-		<link rel="stylesheet" href="<?php echo HOST . DS . $key ?>" media="<?php $value['media'] ?>">
-		<?php else: ?>
-		<link rel="stylesheet" href="<?php echo HOST . DS . $value ?>">
-		<?php endif; ?>
-	<?php endforeach ?>
+		<?php foreach ($foot_css as $key => $value): ?>
+			<?php if(is_array($value)): ?>
+			<link rel="stylesheet" href="<?php echo HOST . DS . $key ?>" media="<?php $value['media'] ?>">
+			<?php else: ?>
+			<link rel="stylesheet" href="<?php echo HOST . DS . $value ?>">
+			<?php endif; ?>
+		<?php endforeach ?>
 	<?php endif; ?>
 
 	<?php if(isset($foot_js)): ?>
-	<?php foreach ($foot_js as $js): ?>
-		<script type="text/javascript" src="<?php echo HOST . DS . $js ?>"></script>
-	<?php endforeach ?>
+		<?php foreach ($foot_js as $js): ?>
+			<script type="text/javascript" src="<?php echo HOST . DS . $js ?>"></script>
+		<?php endforeach ?>
 	<?php endif; ?>
 	
 	<script>
